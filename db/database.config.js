5784dcff8482d5550c43d1eaad5f679015cf6689
Vu_T-Solution <leanhvu86@gmail.com>
2020-02-18 02:38:06 +0700

    db

--- db/database.config.js
+++ db/database.config.js
@@ -1,6 +1,6 @@
-module.exports = {
+/*module.exports = {
     url: 'mongodb://localhost:27017/oauth'
+}*/
+module.exports = {
+    url: 'mongodb://heroku_4b8v5f4t:123456aA@ds215988.mlab.com:15988//heroku_4b8v5f4t'
 }
-// module.exports = {
-//     url: 'mongodb://heroku_4b8v5f4t:123456aA@ds215988.mlab.com:15988//heroku_4b8v5f4t'
-// }
