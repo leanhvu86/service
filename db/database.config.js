d061ab0437e96668a6a2dbcb2285a08cdc68b915
Vu_T-Solution <leanhvu86@gmail.com>
2020-02-18 08:26:57 +0700

    connect db

--- db/database.config.js
+++ db/database.config.js
@@ -2,5 +2,5 @@
     url: 'mongodb://localhost:27017/oauth'
 }*/
 module.exports = {
-    url: 'mongodb://heroku_4b8v5f4t:123456aA@ds215988.mlab.com:15988//heroku_4b8v5f4t'
+    url: 'mongodb://leanhvu:123456aA@ds215988.mlab.com:15988/heroku_4b8v5f4t'
 }
