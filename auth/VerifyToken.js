2c338fe781dc5dfa9ac6f2697177829ba313ab88
Vu_T-Solution <leanhvu86@gmail.com>
2020-04-02 02:32:50 +0700

    Sửa authenication

--- auth/VerifyToken.js
+++ auth/VerifyToken.js
@@ -4,14 +4,15 @@ var config = require('../config'); // get our config file
 function verifyToken(req, res, next) {
 
   // check header or url parameters or post parameters for token
-  var token = req.headers['x-access-token'];
-  if (!token) 
+  const token = req.headers['x-access-token']||req.headers['authorization'];
+  console.log(token)
+  if (!token)
     return res.status(403).send({ auth: false, message: 'No token provided.' });
 
   // verifies secret and checks exp
-  jwt.verify(token, config.secret, function(err, decoded) {      
-    if (err) 
-      return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });    
+  jwt.verify(token, config.secret, function(err, decoded) {
+    if (err)
+      return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
 
     // if everything is good, save to request for use in other routes
     req.userId = decoded.id;
