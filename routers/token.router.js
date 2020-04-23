f7333276ca5d644c2c4aca51e49e746f8d210a5f
Vu_T-Solution <leanhvu86@gmail.com>
2020-04-24 01:26:06 +0700

    bỏ authenication khi delete token

--- routers/token.router.js
+++ routers/token.router.js
@@ -78,7 +78,7 @@ module.exports = app => {
 
   var VerifyToken = require(__root + 'auth/VerifyToken');
   const tokens = require("../controllers/tokens");
-  app.post("/deleteToken",VerifyToken, tokens.deleteToken);
+  app.post("/deleteToken", tokens.deleteToken);
 
   // app.post("/login", users.login);
   // app.post("/test", users.test);
