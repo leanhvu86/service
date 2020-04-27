bfc28e24df4d30365f163bf7f60bd0605ac00584
Vu_T-Solution <leanhvu86@gmail.com>
2020-04-27 14:33:56 +0700

    update

--- routers/recipe.route.js
+++ routers/recipe.route.js
@@ -2,13 +2,14 @@
 module.exports = (app) => {
     const RecipeRouter = require('../controllers/recipe/recipe');
     var VerifyToken = require(__root + 'auth/VerifyToken');
+    var VerifyRoleByToken = require(__root + 'auth/VerifyRoleByToken');
     app.get('/recipes', RecipeRouter.getRecipes);
     app.get('/getAllRecipes', RecipeRouter.getAllRecipes);
     app.get('/findRecipe/:id', RecipeRouter.findRecipe);
     app.post('/createRecipe',VerifyToken, RecipeRouter.createRecipe);
-    app.post('/acceptRecipe',VerifyToken,RecipeRouter.acceptRecipe);
+    app.post('/acceptRecipe',VerifyRoleByToken,VerifyToken,RecipeRouter.acceptRecipe);
     app.get('/getNewRecipes', RecipeRouter.getNewRecipes);
-    app.post('/declineRecipe',VerifyToken, RecipeRouter.declineRecipe);
+    app.post('/declineRecipe',VerifyRoleByToken,VerifyToken, RecipeRouter.declineRecipe);
     app.post('/createMultipleRecipe',VerifyToken, RecipeRouter.createMultiple);
 };
 
